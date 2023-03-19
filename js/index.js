a1226f354bdb7e47f972689e0b30f1783d7cd239
Digooooo <376007191@qq.com>
2023-03-19 18:01:14 +0800

    add third-js and css into index.htm

---
+++ js/index.js
@@ -0,0 +1,16 @@
+
+// 页面加载完之后，方法一
+window.onload = function () {
+
+}
+
+// 方法2二，使用jquery
+$(document).ready(function () {
+    
+})
+
+// 方法三，使用
+$(function (){
+    console.log("mode three")
+    }
+)
